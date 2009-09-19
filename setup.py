c953ebdcbab1a4a42060e8c8823e334c7240289a
Adomas Paltanavičius <adomas@shrubberysoft.com>
2009-09-19 03:58:04 +0300

    Bump up the version number.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@
 from setuptools import setup, find_packages
 
 setup(name='homophony',
-    version='0.1',
+    version='0.1.1',
     description='Django and zc.testbrowser integration',
     long_description=open('README').read(),
     author='Shrubbery Software',
